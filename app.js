faec44def31c8c64eb1ff6c27e69627471138d8b
Kaushal Kumar Singh <117421319+kaushalsinghrajput111@users.noreply.github.com>
2022-12-15 09:08:44 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// alert("hello")
+let btnTranslate=document.querySelector("#btn-translate");
+let input=document.getElementsByTagName("textarea")[0];
+let output=document.getElementsByClassName("output")[0]
+function clickHandler(){
+    output.innerText = input.value; 
+}
+btnTranslate.addEventListener("click",clickHandler)
