c980eda2bd729a44edb884d4fd9bdfa284dbe752
Kaushal Kumar Singh <117421319+kaushalsinghrajput111@users.noreply.github.com>
2022-12-16 09:10:06 +0530

    banana talk is complete

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 let btnTranslate=document.querySelector("#btn-translate");
 let input=document.getElementsByTagName("textarea")[0];
-let output=document.getElementsByClassName("output")[0]
+let output=document.getElementsByClassName("output")[0];
+let serverUrl="	https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(text){
+    return serverUrl + "?" + "text="+ text 
+}
+const errorHandler=(error)=>{
+  console.log("error occured:"+ error)
+  alert("somthing wrong with server")  
+}
+
+
 function clickHandler(){
-    output.innerText = input.value; 
+    // output.innerText = input.value;
+    let inputText =input.value;
+    fetch(getTranslationURL(inputText))
+    .then(response =>response.json())
+    .then(json => {
+let translatedText=json.contents.translated;
+output.innerText = translatedText;
+    })
+    .catch(errorHandler)
 }
 btnTranslate.addEventListener("click",clickHandler)
